782af881e1ef67dbf708e8a84f65a853e3130fe7
mrkyel <kyel100@naver.com>
2021-03-28 20:30:39 +0900

    초기셋팅 준비

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import { Table } from "antd";
+import 'antd/dist/antd.css';
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Chinese Score",
+    dataIndex: "chinese",
+    sorter: {
+      compare: (a, b) => a.chinese - b.chinese,
+      multiple: 3,
+    },
+  },
+  {
+    title: "Math Score",
+    dataIndex: "math",
+    sorter: {
+      compare: (a, b) => a.math - b.math,
+      multiple: 2,
+    },
+  },
+  {
+    title: "English Score",
+    dataIndex: "english",
+    sorter: {
+      compare: (a, b) => a.english - b.english,
+      multiple: 1,
+    },
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    chinese: 98,
+    math: 60,
+    english: 70,
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    chinese: 98,
+    math: 66,
+    english: 89,
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    chinese: 98,
+    math: 90,
+    english: 70,
+  },
+  {
+    key: "4",
+    name: "Jim Red",
+    chinese: 88,
+    math: 99,
+    english: 89,
+  },
+];
 
 function App() {
+  function onChange(pagination, filters, sorter, extra) {
+    console.log("params", pagination, filters, sorter, extra);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainWrapper>
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+      </MainWrapper>
+    </>
   );
 }
 
 export default App;
+
+const MainWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`
